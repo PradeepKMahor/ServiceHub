e8c076b5fc95a6289a12e247e1b1706a55cec6a4
PradeepMahor <PradeepMahor@gmail.com>
2024-07-04 23:10:58 +0530

    Master new Rootc

---
+++ ServiceHub.WebApp/Areas/Masters/Models/Products/RootCauseAnalysis/RootCauseUpdateModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceHub.WebApp.Models
+{
+    public class RootCauseUpdateModel
+    {
+        [Required]
+        [Display(Name = "Root Cause Id")]
+        public string RootCauseId { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Root Cause Code")]
+        public string RootCauseCode { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Root Cause")]
+        public string RootCause { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Product")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required]
+        public string ProductId { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "IsActive")]
+        public string IsActive { get; set; } = string.Empty;
+    }
+}
---
+++ ServiceHub.WebApp/Areas/Masters/Models/Products/SubProduct/SubProductViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceHub.WebApp.Models
+{
+    public class SubProductViewModel
+    {
+        public SubProductViewModel()
+        {
+            this.FilterDataModel = new FilterDataModel();
+        }
+
+        public FilterDataModel FilterDataModel { get; set; }
+
+        [Display(Name = "Product Id")]
+        public string ProductId { get; set; } = string.Empty;
+
+        [Display(Name = "Product Code")]
+        public string ProductCode { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Service Date")]
+        public DateTime? ServiceDate { get; set; }
+
+        [Display(Name = "Warranty Date")]
+        public DateTime? WarrantyDate { get; set; }
+
+        [Display(Name = "Product Description")]
+        public string ProductDescription { get; set; }
+
+        [Display(Name = "Upload Photo")]
+        public string UploadPhoto { get; set; }
+
+        [Display(Name = "IsActive")]
+        public string IsActive { get; set; }
+    }
+}
